d43cf40f71c2bea03aeeffe041bef460320ab05d
mywork <abdala@gmail.com>
2020-04-23 18:07:38 -0500

    still working on it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>Would Your Rather?</h1>
+    <wouldYouRatherQuestion 
+    v-bind:questions="questions"
+    
+    v-on:answer-changed="userAnswers" 
+    ></wouldYouRatherQuestion>
+    <div> 
+      <h1>{{ selectionHeader }}</h1>
+    </div>
+
+    <div> 
+    <p class="user-answers">
+      <span>...? Try making a selection above</span>
+      <ul>
+        <li v-for="choice in userChoices" :key="choice"><span>{{choice}}</span></li>
+      </ul>
+    </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import wouldYouRatherQuestion from './components/wouldYouRatherQuestion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    wouldYouRatherQuestion
+  },
+   data() {
+        return{
+          selectionHeader: 'You Would Rather...',
+          userSelectionMessage: [],
+          userChoices: [],
+            questions: [
+        {
+            id:0,
+            question :'would you want to be a writer or famous?',
+            answer1: 'writer',
+            answer2: 'famous'
+        },
+        {
+            id:1,
+            question :'Would you rather live in a house shaped like a circle or a house shaped like a triangle?',
+            answer1: 'Circle',
+            answer2: 'Triangle'
+        },
+        {
+            id:2,
+            question :'Would you rather be a super hero or wizard?',
+            answer1: 'hero',
+            answer2: 'Wizard'
+        }
+        ],   
+     }
+                
+    },
+    methods: {
+      userAnswers(choice){
+        console.log('msg is emitted')
+        this.userChoices.push(choice)
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
